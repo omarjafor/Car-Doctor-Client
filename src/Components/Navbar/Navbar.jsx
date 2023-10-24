ec2d1177267e8d8a474cf406563882261fd4589b
omarjafor <omarjaforchy@gmail.com>
2023-10-24 23:50:26 +0600

    logo uploaded

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import logo from './../../assets/logo.svg';
 
 
 const Navbar = () => {
@@ -30,7 +31,7 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <Link to="/" className="btn btn-ghost normal-case text-xl">
-                    <img src="" alt="" />
+                    <img src={logo} alt="" />
                 </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
