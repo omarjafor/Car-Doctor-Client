e6e02fdd0967f347a44e23d652ace9499a578a30
omarjafor <omarjaforchy@gmail.com>
2023-10-24 23:29:18 +0600

    car doctor client

--- src/MainLayout/Root.jsx
+++ src/MainLayout/Root.jsx
@@ -4,7 +4,6 @@ import { Outlet } from "react-router-dom";
 const Root = () => {
     return (
         <div>
-            <div> <h1 className="text-3xl font-bold text-center">Hello world!</h1></div>
             <Outlet></Outlet>
         </div>
     );
