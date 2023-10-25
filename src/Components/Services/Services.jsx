85252ec41b1fa7ed259afe70555d62fdc9cabbe0
omarjafor <omarjaforchy@gmail.com>
2023-10-25 16:51:55 +0600

    services data load

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
 
 
 const Services = () => {
@@ -9,7 +10,10 @@ const Services = () => {
         .then(res => res.json())
         .then(data => setServices(data))
     } , [])
-    
+
+    const handleSearch = () => {
+
+    }
     return (
         <div className="mt-4">
             <div className="text-center">
@@ -18,7 +22,7 @@ const Services = () => {
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
                 <div className="form-control">
                     <div className="input-group">
-                        <input type="text" ref={searchRef} placeholder="Search…" className="input input-bordered" />
+                        <input type="text"  placeholder="Search…" className="input input-bordered" />
                         <button onClick={handleSearch} className="btn btn-square">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                         </button>
@@ -26,8 +30,7 @@ const Services = () => {
                 </div>
                 <button
                     className="btn btn-primary"
-                    onClick={() => setAsc(!asc)}
-                >{asc ? 'Price: High to Low' : 'Price: Low to High'}</button>
+                >Price</button>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
