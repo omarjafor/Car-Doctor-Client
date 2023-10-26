d0987ffd3aef42a923de5d6aab4136e7090276d4
omarjafor <omarjaforchy@gmail.com>
2023-10-26 23:09:56 +0600

    some practice client side

--- src/Components/BookService/BookService.jsx
+++ src/Components/BookService/BookService.jsx
@@ -22,17 +22,7 @@ const BookService = () => {
             price: price
         }
 
-        fetch('http://localhost:5000/bookings', {
-            method: 'POST',
-            headers: {
-                'content-type' : 'application/json'
-            },
-            body: JSON.stringify(booking)
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-        })
+        
     }
     return (
         <div>
