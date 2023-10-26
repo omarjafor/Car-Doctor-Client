cb640ca0048732470a2d002e4f2371a7085e24ff
omarjafor <omarjaforchy@gmail.com>
2023-10-26 22:09:12 +0600

    some components modified

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -6,14 +6,12 @@ const Services = () => {
     const [services, setServices] = useState([]);
 
     useEffect( () => {
-        fetch('services.json')
+        fetch('http://localhost:5000/services')
         .then(res => res.json())
         .then(data => setServices(data))
     } , [])
 
-    const handleSearch = () => {
-
-    }
+    
     return (
         <div className="mt-4">
             <div className="text-center">
@@ -23,7 +21,7 @@ const Services = () => {
                 <div className="form-control">
                     <div className="input-group">
                         <input type="text"  placeholder="Search…" className="input input-bordered" />
-                        <button onClick={handleSearch} className="btn btn-square">
+                        <button className="btn btn-square">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                         </button>
                     </div>
