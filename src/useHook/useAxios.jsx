6af16b042f2b88bf5e9e4cfa835566439256fe7b
omarjafor <omarjaforchy@gmail.com>
2023-10-31 02:29:02 +0600

    fake user or token logout

--- src/useHook/useAxios.jsx
+++ src/useHook/useAxios.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useHook from "./useHook";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000/',
@@ -6,6 +9,26 @@ const axiosSecure = axios.create({
 })
 
 const useAxios = () => {
+    const { logOut } = useHook();
+    const navigate = useNavigate()
+
+    useEffect( () => {
+        axiosSecure.interceptors.response.use( res => {
+            return res;
+        }, error => {
+            console.log('Error Tracking', error);
+            if(error.response.status === 401 || error.response.status === 403){
+                console.log('Logout the user');
+                logOut()
+                .then(() => {
+                    navigate('/login')
+                })
+                .catch(err => console.log(err.message))
+            }
+        })
+
+    } , [logOut, navigate])
+
     return axiosSecure;
 };
 
